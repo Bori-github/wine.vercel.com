61e4bf9bb7124a487ba66484a2bbaee9f58c414a
Bori-github <85009583+Bori-github@users.noreply.github.com>
2021-12-19 20:56:04 +0900

    🚀 Wine 페이지 생성

---
+++ src/pages/wine.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+// Q. import 가 뭔가요? 몰라도 됨
+
+const WinePage: NextPage = () => {
+  return <div>Wine</div>;
+};
+
+export default WinePage;
+// Q. export 가 뭔가요? 몰라도 됨
